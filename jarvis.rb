fedcf24cad5aaad82ada53578371b5dd148dc1cc
leandrosardi <leandro.sardi@expandedventure.com>
2024-03-30 22:03:12 -0300

    renaming constant DB_REFRESH_TOKEN

--- jarvis.rb
+++ jarvis.rb
@@ -7,7 +7,7 @@ client = BlackStack::Jarvis.new(
     openai_api_key: OPENAI_API_KEY,
     openai_model: OPENAI_MODEL,
     adspower_api_key: ADSPOWER_API_KEY,
-    dropbox_refresh_token: DB_REFRESH_TOKEN,
+    dropbox_refresh_token: DROPBOX_REFRESH_TOKEN,
 )
 
 client.console
