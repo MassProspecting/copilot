b98ca51e48f693d1f60b48789650a96851690e23
leandrosardi <leandro.sardi@expandedventure.com>
2024-06-29 12:12:05 -0300

    examples

--- examples/test.rb
+++ examples/test.rb
@@ -6,17 +6,21 @@
 # 
 
 require 'colorize'
+require 'pry'
 require 'openai' #, '~>6.3.1'
 require 'config'
 
-client = OpenAI::Client.new(access_token: OPENAI_API_KEY)
-#client = OpenAI::Client.new(access_token: 'dfdf') # enable this line to make it fail
+#client = OpenAI::Client.new(access_token: OPENAI_API_KEY)
+client = OpenAI::Client.new(access_token: 'dfdf') # enable this line to make it fail
 
 puts "OpenAI API Key: #{client.access_token.to_s.blue}"
-
+begin
 puts client.chat(
     parameters: {
         model: OPENAI_MODEL, # Required.
         messages: [{ role: "user", content: "Hello!"}], # Required.
         temperature: 0.7,
 })
+rescue => e
+binding.pry
+end
